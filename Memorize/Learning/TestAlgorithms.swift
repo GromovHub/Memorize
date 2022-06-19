641b904cca6df2b67226494c261f0127710c724f
Vitaly Gromov <64252506+GromovHub@users.noreply.github.com>
2022-06-19 21:12:53 +0200

    linear search

--- Memorize/Learning/TestAlgorithms.swift
+++ Memorize/Learning/TestAlgorithms.swift
@@ -33,5 +33,10 @@ struct TestAlgorithms {
       return array
     }
     
-    
+    public func linearSearch<T: Equatable>(_ array: [T], _ object: T) -> Int? {
+       for (index, obj) in array.enumerated() where obj == object {
+    return index
+  }
+  return nil
+}
 }
