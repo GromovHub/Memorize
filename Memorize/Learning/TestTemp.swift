0c8f1cc1494cd3a9ac147fdad8dfd33551900442
Vitaly Gromov <gromovitaly@gmail.com>
2022-06-27 15:45:08 +0300

    git test and alg

--- Memorize/Learning/TestTemp.swift
+++ Memorize/Learning/TestTemp.swift
@@ -336,4 +336,26 @@ struct TestTemp {
         print(x.changeGear(up: true, down: true))
         print(x.currentGear)
     }
+    
+    extension String {
+      func indexOf(_ pattern: String) -> String.Index? {
+        for i in self.characters.indices {
+            var j = i
+            var found = true
+            for p in pattern.characters.indices{
+                if j == self.characters.endIndex || self[j] != pattern[p] {
+                    found = false
+                    break
+                } else {
+                    j = self.characters.index(after: j)
+                }
+            }
+            if found {
+                return i
+            }
+        }
+        return nil
+      }
+    }
+    
 }
