48dd61523205d7ba013ffe0975d587b7e1b76d5e
Vitaly Gromov <gromovitaly@gmail.com>
2022-06-28 15:07:20 +0300

    git course and insertion sort

--- Memorize/Learning/TestAlgorithms.swift
+++ Memorize/Learning/TestAlgorithms.swift
@@ -135,4 +135,16 @@ struct TestAlgorithms {
         return Array(a[0..<k])
     }
     
+    func insertionSort(_ array: [Int]) -> [Int] {
+        var sortedArray = array             // 1
+        for index in 1..<sortedArray.count {         // 2
+            var currentIndex = index
+            while currentIndex > 0 && sortedArray[currentIndex] < sortedArray[currentIndex - 1] { // 3
+                sortedArray.swapAt(currentIndex - 1, currentIndex)
+                currentIndex -= 1
+            }
+        }
+        return sortedArray
+    }
+
 }
--- Memorize/Learning/TestTemp.swift
+++ Memorize/Learning/TestTemp.swift
@@ -337,25 +337,4 @@ struct TestTemp {
         print(x.currentGear)
     }
     
-    extension String {
-      func indexOf(_ pattern: String) -> String.Index? {
-        for i in self.characters.indices {
-            var j = i
-            var found = true
-            for p in pattern.characters.indices{
-                if j == self.characters.endIndex || self[j] != pattern[p] {
-                    found = false
-                    break
-                } else {
-                    j = self.characters.index(after: j)
-                }
-            }
-            if found {
-                return i
-            }
-        }
-        return nil
-      }
-    }
-    
 }
